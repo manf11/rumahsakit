0e2ab8a3e2a5b01bb91b551e2da7f81aac57d97a
manf11 <mohahlisnoorfuad1@gmail.com>
2020-12-25 16:03:51 +0700

    menambahkan index.php

--- kontakkami.php
+++ kontakkami.php
@@ -14,7 +14,7 @@
   <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
-  <a class="navbar-brand" href="#">
+  <a class="navbar-brand active" href="#">
       <img src="logo.jpg" alt="" width="45" height="40">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -23,7 +23,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item ">
-          <a class="nav-link " aria-current="page" href="#">Beranda</a>
+          <a class="nav-link " aria-current="page" href="index.php">Beranda</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="kontakkami.php">Kontak kami</a>
@@ -45,41 +45,26 @@
     </div>
   </div>
 </nav>
-<div class="container">
-      <div class="card mt-2">
-        <h2 class="card-header ">FORM PENDAFTARAN </h2>
-          <div class="card-body"></
-          <form class="row g-3">
-  <div class="col-md-6">
-    <label for="namapasien" class="form-label">Nama Pasien</label>
-    <input type="text" class="form-control" id="namapasien">
-  </div>
-  <div class="col-md-6">
-    <label for="inputPassword4" class="form-label">Password</label>
-    <input type="password" class="form-control" id="inputPassword4">
-  </div>
-  <div class="col-12">
-    <label for="inputAddress2" class="form-label">alamat</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="isi alamat">
-  </div>
-  <div class="col-md-6">
-    <label for="inputCity" class="form-label">penyakit</label>
-    <input type="text" class="form-control" id="inputCity">
-  <div class="col-12 mt-2">
-    <button type="submit" class="btn btn-primary">Daftar</button>
-  </div>
-</form>
-              </div>
-              </div>
-          </form>
-        </div>
+<br>
+<div class="card mb-3" style="max-width: 500px;">
+  <div class="row no-gutters">
+    <div class="col-md-4">
+      <img src="WhatsApp Image 2020-02-25 at 19.29.57 copy.jpeg.jpg" class="card-img" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Kartu nama</h5>
+        <h6>Moh Ahlis Nor Fuad
+        <p>Project Manager
+        <br>mohahlisnoorfuad1@gmail.com
+        <br>+62 8211 5451 298
+        <br>
+        <br>PT Fino Technical</p>
       </div>
     </div>
   </div>
 </div>
 </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 
-
-  </body>
+</body>
 </html>
